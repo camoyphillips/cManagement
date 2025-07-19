d6a908728f5bbb3995d65e84e55a3b26083215f8
Camoy Phillips <n01496384@humber.ca>
2025-07-19 02:56:47 -0400

    Add project files.

---
+++ cManagement/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using cManagement.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace cManagement.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
